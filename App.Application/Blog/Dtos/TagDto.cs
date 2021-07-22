9bae7bc79571657b9ae59537790873aa1e532639
Administrator <Administrator@DESKTOP-F89DO8O>
2021-07-22 14:49:47 +0800

    添加项目文件。

---
+++ App.Application/Blog/Dtos/TagDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace App.Application.Blog.Dtos
+{
+    [Serializable]
+    public class TagDto : EntityDto<string>
+    {
+        public string TagName { get; set; }
+
+        public string BGColor { get; set; }
+
+        public string Id { get; set; }
+    }
+}
---
+++ App.Application/Blog/ICategoryService.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using App.Application.Blog.Dtos;
+using App.Core.Entities.Blog;
+using App.Framwork.Result;
+
+namespace App.Application.Blog
+{
+    public interface ICategoryService : IAppService<CategoryInfo>
+    {
+        /// <summary>
+        /// 新增修改栏目
+        /// </summary>
+        /// <param name="dto"></param>
+        /// <returns></returns>
+        Task<UnifyResult> Save(CategoryInputDto dto);
+
+        /// <summary>
+        /// 获取所有一级分类
+        /// </summary>
+        /// <returns></returns>
+        Task<List<CategoryDto>> GetRootCategories();
+
+        /// <summary>
+        /// 获取该目录下所有子目录，如果没有子目录，则返回上一级目录的子目录
+        /// </summary>
+        /// <param name="cid"></param>
+        /// <returns></returns>
+        Task<List<CategoryDto>> GetRootCategoriesByCid(string cid);
+    }
+}
